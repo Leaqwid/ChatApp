34cd8e69fc063ab62a57e20643ae941a64bc8e63
Leaqwid <tambarobert2517@gmail.com>
2026-05-07 13:09:04 +0300

    Add user route with status codes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,13 +2,31 @@ import express from 'express'
 const app= express();
 
 app.get('/', (req,res) => {
-    res.send('Hello from ChatApp');
+    const url = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+    console.log("Adresa website-ului: " + url);
+    res.json({message:"Hello and welcome !"})
+})
+
+app.get('/api/user/:id' ,(req,res) => {
+    if (req.params.id === '123') {
+        res.status(200).json({ 
+            id: '123',
+            username: 'Leaqwid',
+            email: 'test@test.com'
+        });
+    }else {
+        res.status(404).json({ message : 'Not found'})
+    }
+    
 })
 
 app.get('/health' ,(req,res)=> {
-    res.send("Server is running ...")
+    res.json({status:"Succes" , message: "Server is running succesfuly" })
 } )
 
+
 app.listen(3000 , () => {
+    const url = "http://localhost:3000";
+    console.log("Server open on " + url);
     console.log("Express is working.Port open on 3000 ");
 });
